82c70db066663c93e491e4456f35939f03b6cd1b
Kadin <kadinvanvalin@yahoo.com>
2017-02-07 06:16:22 -0500

    wireframe works

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-  angular.module('diveLog', [])
-      .controller('diveLogCtrl', DiveLogCtrl);
+  angular.module('quizApp', [])
+      .controller('quizAppCtrl', quizAppCtrl);
     
-    function DiveLogCtrl($scope) {
+    function quizAppCtrl($scope) {
       $scope.questions = questions;
       $scope.quizNum = quizNum;
       $scope.correctAnswers = correctAnswers;
@@ -10,6 +10,24 @@
       function setView(view){
       	$scope.view = view;
       }
+      function shuffle(array) {
+  var m = array.length, t, i;
+
+  // While there remain elements to shuffle…
+  while (m) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+
+  return array;
+}
+
      
       $scope.startQuiz = function(){
       	setView('quiz');
@@ -24,11 +42,17 @@
       } 
       $scope.nextQuestion = function(){
          if($scope.test == 'correct'){
-          $scope.correctAnswers = correctAnswers++; 
+          $scope.correctAnswers++; 
         }
-        if($scope.quizNum === 4){setView('post');}
-        $scope.quizNum = quizNum++;
+        if($scope.quizNum >= 4){setView('post');}
+        $scope.quizNum++;
         console.log(quizNum);
+      }
+      $scope.restart = function(){
+        setView('preQuiz');
+        $scope.quizNum = 0;
+        $scope.correctAnswers = 0;
+        
       }
       setView('preQuiz');
       console.log($scope.view);
--- state.js
+++ state.js
@@ -2,30 +2,35 @@ var questions = [
   {
     question: 'What device did Ender use to destroy the formic homeworld',
     answers: ["The little Doctor device", "A nuclear missle", "His charming personality", "he didn't destory it"],
+    correctAns:"The little Doctor device",
     depth: 72,
     time: 54
   },
   {
    question: 'Who said in the final battle, the enemy gate is down',
     answers: ["Bean", "Petra", "Ender", "Graff"],
+    correctAns:"Bean",
     depth: 54,
     time: 38
   },
   {
     question: 'How is Mazer still alive?',
     answers: ["The miracle of relativity", "They cloned him", "He was frozen in a ship in space", "Large doses of ITworks"],
+    correctAns:"The miracle of relativity",
     depth: 54,
     time: 38
   },
   {
    question: 'What is the first army Ender is assigned to?',
     answers: ["Salamander", "Rat", "Rabbit", "Dragon"],
+    correctAns:"Salamander",
     depth: 54,
     time: 38
   },
   {
    question: 'Commander Graff is so stressed out during the events of the book he did what?',
     answers: ["Gained weight", "Started killing the children for fun", "Lost his hair", "Drank constantly"],
+    correctAns:"Gained weight",
     depth: 54,
     time: 38
   }
